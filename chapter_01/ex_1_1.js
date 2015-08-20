92a40668384c7fe2d0c6f3776da8208e62e2a0e0
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-08-20 17:25:23 -0600

    two solutions for ex 1.1

--- chapter_01/ex_1_1.js
+++ chapter_01/ex_1_1.js
@@ -1,22 +1,37 @@
 var string1 = "hello"
 var string2 = "world"
 
-function unique (input) {
-  // for (var i = 1; i < input.length; i++) {
-  //   if (input[i] === input[i-1]) {
-  //     return "Not unique"
-  //   }
-  // }
-  if (i === input.length) {
-    return "done!"
-  }
-  var i = 1
-  if (input[i] === input[i-1]) {
-    return "Not unique"
-  } else {
-    unique(input)
+
+// For loop solution:
+function uniqueForLoop (input) {
+  for (var i = 1; i < input.length; i++) {
+    if (input[i] === input[i-1]) {
+      return "Not unique"
+    }
   }
 }
 
-console.log(unique(string1));
-console.log(unique(string2));
+console.log(uniqueForLoop(string1));
+console.log(uniqueForLoop(string2));
+
+// Recursive solution:
+
+var i = 1
+function uniqueRecursive (input) {
+  if (i === input.length) {
+    console.log("All of this word's letters are unique.");
+    return true
+  }
+  console.log("input[i]", input[i]);
+  console.log("input[i-1]", input[i-1]);
+  if (input[i] === input[i-1]) {
+    console.log("This word has duplicated letters.");
+    return false
+  } else {
+    i++
+    uniqueRecursive(input)
+  }
+}
+
+console.log(uniqueRecursive(string1));
+console.log(uniqueRecursive(string2));
